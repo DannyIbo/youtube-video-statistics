979d90e8394dad293d32a67afc633fc2950463cd
danny <danny.ibovnik@gmail.com>
2019-10-26 17:28:48 +0200

    Upload a first simple running version

---
+++ config/EXAMPLE_config .py
@@ -0,0 +1,10 @@
+# This is an example of a config.py.
+# Please follow theses steps:
+# 1. Place yout YouTube Data API key in this file.
+# 2. Rename this file from 'EXAMPLE_config.py' to 'config.py'
+
+config = {
+    'YouTube':{
+        'YOUTUBE_API' : 'PLACE YOUR YOUTUBE DATA API HERE AS A STRING'
+    }
+}
---
+++ youtube_video_statistics.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+# This file contains credentials like your YouTube API
+from config import config
+# This module contains the main function to make this code work
+from docs import youtube_data_module_10 as ytd
+
+# Get your YouTube API.
+YOUTUBE_API = config.config.get('YouTube').get('YOUTUBE_API')
+
+# Create credential object to use the YouTube API
+youtube = ytd.youtubeAPIkey(YOUTUBE_API)
+
+# Set the channel you want to get the video statistics for
+# channelId = 'UCV9pZxcKWF6fZ1ZQzDWofgw'
+# Set the channel Id hard coded...
+channelId = ''
+# ... or get the channel Id by user input
+if not channelId:
+    channelId = input('Please enter the channel Id you like to get video statistics for:')
+
+# Get a list of video Ids that are publicly available on the given channel
+try:
+    video_id_list = ytd.videoIdList(youtube, channelId)
+except:
+    print('Error: This may not be a valid YouTube channel Id or there are no videos publicly available. \nPlease find a valid channel Id in the URL. For example: "UCV9pZxcKWF6fZ1ZQzDWofgw" is the channel Id in the URL "https://www.youtube.com/channel/UCV9pZxcKWF6fZ1ZQzDWofgw"')
+
+# Get video statisics and several other figures and return a .json-file
+video_snippets = ytd.video_snippets(youtube, video_id_list)
+
+# Restructure the .json-file and extract only the needed data
+video_snippet_dict = ytd.snippets_to_dict(video_snippets, yt_credentials=youtube)
+
+# Create a pandas Data Frame
+df = pd.DataFrame(video_snippet_dict).set_index('videoId')
+
+# Write data frame to excel
+df_excel = df.drop(columns=['date_data_created'])
+df_excel.to_excel(f'output/youtube-video-statistics_channel_Id_{channelId}.xlsx')
+
+print(f'Successfully downloaded YouTube video statistics for channel Id "{channelId}". \nPlease check the output folder for the final .xlsx-file.')
