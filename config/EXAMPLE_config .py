5a2542aef08ba6c1a12f305f00eca6ee01a92d02
DannyIbo <54529853+DannyIbo@users.noreply.github.com>
2019-10-26 17:35:39 +0200

    Update EXAMPLE_config.py

--- config/EXAMPLE_config .py
+++ config/EXAMPLE_config .py
@@ -1,6 +1,6 @@
 # This is an example of a config.py.
 # Please follow theses steps:
-# 1. Place yout YouTube Data API key in this file.
+# 1. Place your YouTube Data API key in this file. If you do not have an YouTube data API key yet and want to know how to create one, plese visit https://www.slickremix.com/docs/get-api-key-for-youtube/
 # 2. Rename this file from 'EXAMPLE_config.py' to 'config.py'
 
 config = {
